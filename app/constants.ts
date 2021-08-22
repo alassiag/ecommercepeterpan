9e1f7f38b735331cf6c9b9abb9b993cc58b5b31d
Alassiag <alassiag@gmail.com>
2021-08-22 13:17:24 -0300

    colors test

--- app/constants.ts
+++ app/constants.ts
@@ -4,9 +4,8 @@ export const INFORMATION = {
   title: "PeterPan",
   description: "Reposteria & Cotillon",
   phone: "5493468515731",
+  color: "pink",
   sheet:
-    // "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIaDn6oeqSzhctjPrgqQPgglunYTFkDcEeyLiOzbyfLyd1CGMii0MVGZYzV_1cu5EQoIt2AeOh0uLA/pub?gid=0&single=true&output=csv",
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vT3Z_uVkybdGO1oiWyg_1wnOYXg1SNAVtJRLzYEczKakgXY--lJYXeFxZVqLv6xyAbSRGuGJfk5ZimL/pub?output=csv",
-
-  color: "teal",
 };
+// "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIaDn6oeqSzhctjPrgqQPgglunYTFkDcEeyLiOzbyfLyd1CGMii0MVGZYzV_1cu5EQoIt2AeOh0uLA/pub?gid=0&single=true&output=csv",
